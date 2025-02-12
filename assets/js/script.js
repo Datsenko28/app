e8680fdf5dad4627f912929aed9553909d03082a
Ihor Datsenko <datsenko2802@gmail.com>
2025-02-12 20:41:05 +0100

    first 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,6 +54,7 @@ document.addEventListener("DOMContentLoaded", () => {
     currentIndex = (currentIndex - 1 + cards.length) % cards.length; // Зациклення назад
     updateSlider();
     addClass(prevButton, "active");
+    console.log("123");
   });
 
   function addClass(element, className) {
